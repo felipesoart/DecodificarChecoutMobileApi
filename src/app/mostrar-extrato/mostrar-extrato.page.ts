8c6b2ea6c6bc2107c7410e32b7cd7e934df45f77
felipesoart <48894718+felipesoart@users.noreply.github.com>
2020-06-23 12:29:25 -0300

    carregar bandeiras - agustado e funciomando tanto para debito quanto credito

--- src/app/mostrar-extrato/mostrar-extrato.page.ts
+++ src/app/mostrar-extrato/mostrar-extrato.page.ts
@@ -23,14 +23,34 @@ export class MostrarExtratoPage implements OnInit {
   Status: string = "";
   ValorLiquido: number;
   DataVencimento: string = "";
-  CValue:String;
-  meioPagamento: string = "";
-  parcela: String;
+  CValue: String;
+  
+  
   
 
-
+  meioPagamento: string = "";
   meioPagamentos: any = [];
+
+  parcela: String;
   parcelas: Array<Number> = new Array<Number>();
+
+  bandeira: String;
+  bandeiras: any = [];
+  bandeirasCredito: any = [
+    {nomeBandeira: 'Visa'},
+    {nomeBandeira: 'MasterCard'},
+    {nomeBandeira: 'Elo'},
+    {nomeBandeira: 'Amex'},
+    {nomeBandeira: 'Dines'},
+    {nomeBandeira: 'Discover'},
+    {nomeBandeira: 'Aura'},
+    {nomeBandeira: 'JCB'},
+  ];
+  bandeirasDebito: any = [
+    {nomeBandeira: 'Visa'},
+    {nomeBandeira: 'MasterCard'}
+  ]
+
 
 
 
@@ -38,9 +58,9 @@ export class MostrarExtratoPage implements OnInit {
   /* variaveis de pagamento */
   nomeCartao: string = "";
   nomeRA: string = "Joao Paulo Felipe Sobrinho de Souza";
-  
-  bandeira: any[];
- 
+
+
+
   validade: string = "";
   numeroCartao: string = "";
   codSeguranca: number;
@@ -58,7 +78,7 @@ export class MostrarExtratoPage implements OnInit {
       this.CodFilial = data.CodFilial;
       this.Servico = data.Servico;
       this.IdBoleto = data.IdBoleto;
-      this.CodServicoPrincipal = data.CodServicoPrincipal;      
+      this.CodServicoPrincipal = data.CodServicoPrincipal;
       this.Status = data.Status;
       this.ValorLiquido = data.ValorLiquido;
       this.DataVencimento = data.DataVencimento;
@@ -66,13 +86,14 @@ export class MostrarExtratoPage implements OnInit {
     });
 
     this.carregarFormasPagamento();
-        
+
   }
 
   onChange(CValue) {
-    this.meioPagamento=CValue;
-        this.carregarTemplatePagamento();
-}
+    this.meioPagamento = CValue;
+    this.carregarTemplatePagamento();
+    this.carragarBandeira();
+  }
 
   async presentToast() {
     const toast = await this.toastController.create({
@@ -83,65 +104,81 @@ export class MostrarExtratoPage implements OnInit {
     toast.present();
   }
 
- /*   btPagamento() {
+  /*   btPagamento() {
+     return new Promise(resolve => {
+       let dados = {
+         requisicao: 'add',
+         nomeCartao: this.nomeCartao,
+         nomeRA: this.nomeRA,
+         meioPagamento: this.meioPagamento,
+         bandeira: this.bandeira,
+         parcelas: this.parcelas,
+         validade: this.validade,
+         numeroCartao: this.numeroCartao,
+         codSeguranca: this.codSeguranca
+         
+       };
+       this.presentToast();
+       console.log(dados);
+    
+ 
+     });
+   }  
+   */
+
+  carregarFormasPagamento() {
+
     return new Promise(resolve => {
-      let dados = {
-        requisicao: 'add',
-        nomeCartao: this.nomeCartao,
-        nomeRA: this.nomeRA,
-        meioPagamento: this.meioPagamento,
-        bandeira: this.bandeira,
-        parcelas: this.parcelas,
-        validade: this.validade,
-        numeroCartao: this.numeroCartao,
-        codSeguranca: this.codSeguranca
-        
-      };
-      this.presentToast();
-      console.log(dados);
-   
-
-    });
-  }  
-  */
-
-  carregarFormasPagamento(){
-
-    return new Promise(resolve => {
-    this.provider.ApiGet(`api/Pagamento/GetFormasPagamento/${this.CodColigada}/${this.CodFilial}/${this.CodServicoPrincipal}`)
+      this.provider.ApiGet(`api/Pagamento/GetFormasPagamento/${this.CodColigada}/${this.CodFilial}/${this.CodServicoPrincipal}`)
         .subscribe(data => {
-          console.log(data)
-          for (let f of data['Response']['FormasPagamento']) {
-            this.meioPagamentos.push(f);
+          /* console.log(data) */
+          for (let mp of data['Response']['FormasPagamento']) {
+            this.meioPagamentos.push(mp);
             err => console.log(err)
           }
           resolve(true);
-          
+
         });
     });
   }
 
   /* let variavel: number=this.parcela as number; explo de corverta string em numero */
 
-  carregarTemplatePagamento(){
-    this.parcela="";
-    this.parcelas=[]
+  carregarTemplatePagamento() {
+    this.parcela = "";
+    this.parcelas = []
     return new Promise(resolve => {
-    this.provider.ApiGet(`api/Pagamento/GetTemplatePagamento/${this.meioPagamento}`)
+      this.provider.ApiGet(`api/Pagamento/GetTemplatePagamento/${this.meioPagamento}`)
         .subscribe(data => {
-        
-          let minParcela=data['Response']['TemplatePagamento']['MinimoParcelas']
-          let maxParcela=data['Response']['TemplatePagamento']['MaximoParcelas']
+
+          let minParcela = data['Response']['TemplatePagamento']['MinimoParcelas']
+          let maxParcela = data['Response']['TemplatePagamento']['MaximoParcelas']
           for (let p = minParcela; p <= maxParcela; p++) {
-            this.parcelas.push(p);            
+            this.parcelas.push(p);
           }
           /* console.log(data['Response']['TemplatePagamento']['MaximoParcelas']) */
-          
+
           resolve(true);
-          
+
         });
     });
   }
+  carragarBandeira() {
+    this.bandeira = "";
+    this.bandeiras=[];
+    return new Promise(resolve => {      
+      if (this.meioPagamento == '1063') {
+      /*   console.log(this.bandeiras=this.bandeirasCredito) */
+        this.bandeiras=this.bandeirasCredito;        
+      }
+      if (this.meioPagamento == '1064') {
+        /* console.log(this.bandeiras=this.bandeirasDebito) */
+        this.bandeiras=this.bandeirasDebito;
+      }
+      resolve(true);
+
+    });
+  }
+
 
 }
-
